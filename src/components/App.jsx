242d556541ca21c5222ed317fd1bd596dff22666
Anastasiia-Ablieieva <110480323+Anastasiia-Ablieieva@users.noreply.github.com>
2023-06-08 23:07:34 +0300

    redux-toolkit-reselect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,26 @@
-import { Layout } from "components/Layout/Layout";
-import { AppBar } from "components/AppBar/AppBar";
-import { TaskForm } from "components/TaskForm/TaskForm";
-import { TaskList } from "components/TaskList/TaskList";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchTasks } from "redux/operations";
+import { TaskList } from "./TaskList/TaskList";
+import { selectError, selectIsLoading } from "redux/selectors";
+import { TaskForm } from "./TaskForm/TaskForm";
+import { AppBar } from "./AppBar/AppBar";
+import { Layout } from "./Layout/Layout";
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
   return (
     <Layout>
       <AppBar />
       <TaskForm />
+      {isLoading && !error && <p>Request in progress...</p>}
       <TaskList />
     </Layout>
   );
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,21 +1,21 @@
 import { MdClose } from "react-icons/md";
 import { useDispatch } from "react-redux";
-import { deleteTask, toggleCompleted } from "redux/tasksSlice";
+import { deleteTask, toggleCompleted } from "redux/operations";
 import css from "./Task.module.css";
 
 export const Task = ({ task }) => {
-
   const dispatch = useDispatch();
+
   const handleDelete = () => dispatch(deleteTask(task.id));
-  const handleToggle = () => dispatch(toggleCompleted(task.id));
+  const handleToggle = () => dispatch(toggleCompleted(task));
 
   return (
     <div className={css.wrapper}>
       <input
         type="checkbox"
         className={css.checkbox}
-        onClick={handleToggle}
         checked={task.completed}
+        onChange={handleToggle}
       />
       <p className={css.text}>{task.text}</p>
       <button className={css.btn} onClick={handleDelete}>
